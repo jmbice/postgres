292dc0255dcf3598aceaebe68523a25c7805c544
Jordan Bice <jmbice@gmail.com>
2018-08-13 11:27:42 -0700

    Update create_copy_addFK_fromCSV.sql

--- uploadBulk/create_copy_addFK_fromCSV.sql
+++ uploadBulk/create_copy_addFK_fromCSV.sql
@@ -18,11 +18,10 @@ CREATE TABLE comments (
 --\copy the data from CSV file to DB
   --note: \copy  = non super user, while COPY is exclusively for super user
   --note: you must use an absolute path for COPY and \copy
-\copy songs(name,length) FROM '/Users/jordan_bice/documents/hack-reactor/sdc/Postgres/testCases/songs_testData.csv' DELIMITER ',' CSV HEADER;
-\copy comments(song_id,author,avatar,date_written,time_written,text_written) FROM '/Users/jordan_bice/documents/hack-reactor/sdc/Postgres/testCases/comments_testData.csv' DELIMITER ',' CSV HEADER;
+\copy songs(name,length) FROM 'your/file/path/data.csv' DELIMITER ',' CSV HEADER;
+\copy comments(song_id,author,avatar,date_written,time_written,text_written) FROM 'your/other/file/path/data.csv' DELIMITER ',' CSV HEADER;
 
-
---this is where you might add indeces if you decided to ommit them. For an example of the code, reference the example section on the bottom of this page: https://www.postgresql.org/docs/9.1/static/sql-altertable.html
+--this is where you might add indeces if you had to ommitted them prior. For an example of the code, reference the example section on the bottom of this page: https://www.postgresql.org/docs/9.1/static/sql-altertable.html
 
 
 --add foreing keys from comments to songs:
